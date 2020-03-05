8db63be1344965fc20163e7536ca1bb9762a64c5
Marcial Garcia <garcia.dfe@gmail.com>
2020-03-05 18:00:46 -0300

    Create Services List section

--- src/pages/ReparosMecanicos/index.js
+++ src/pages/ReparosMecanicos/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { useStaticQuery, graphql } from 'gatsby';
+import Img from "gatsby-image"
 
-//import { } from "./styles";
+import { 
+   ServicesListContainer,
+   Side,
+   ServicesTitleSection,
+   ServicesListSection,
+   ListServices,
+   ListIcon } from "./styles";
 
 import Layout from "../../components/Layout";
 import SEO from "../../components/seo";
 import TopBar from "../../components/TopBar";
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
+import Section from "../../objects/Section"
 import Footer from "../../components/Footer";
 
 const ReparosMecanicos = () => {
@@ -16,6 +24,13 @@ const ReparosMecanicos = () => {
       graphql`
          query {
             imgBanner: file(relativePath: { eq: "banner/banner-reparos-mecanicos.jpg" }) {
+               childImageSharp {
+                  fluid(maxWidth: 1440) {
+                     ...GatsbyImageSharpFluid_tracedSVG
+                  }
+               }
+            }
+            imgService: file(relativePath: { eq: "reparos-mecanicos.jpg" }) {
                childImageSharp {
                   fluid(maxWidth: 1440) {
                      ...GatsbyImageSharpFluid_tracedSVG
@@ -38,6 +53,44 @@ const ReparosMecanicos = () => {
             imageFluid={images.imgBanner.childImageSharp.fluid}
             imageAlt="imagem de motor de polia de tração de elevador"    
          />
+         <Section>
+            <ServicesListContainer>
+               <Side className="-content">
+                  <ServicesTitleSection
+                     title="Serviços"
+                     subtitle="de Reparos Mecânicos"
+                  />
+                  <ServicesListSection>
+                     <ListServices>
+                        <li><ListIcon />
+                           Troca de cabos e polias de tração. Instalação de novas máquinas;
+                        </li>
+                        <li><ListIcon />
+                           Máquinas de tração, motores, freios: eliminação de ruídos, vazamentos e ajustes de folgas;
+                        </li>
+                        <li><ListIcon />
+                           Limitador de velocidade: serviços de recondicionamento, aferição e trocas do conjunto;
+                        </li>
+                        <li><ListIcon />
+                           Retífica da polia de tração, recondicionamento de polias, desvios e eixos;
+                        </li>
+                        <li><ListIcon />
+                           Alinhamento de guias, troca das corrediças, encurtamento, equalização de cabos de tração;
+                        </li>
+                        <li><ListIcon />
+                           Elevadores hidráulicos: reparo dos pistões e geral;
+                        </li>
+                     </ListServices>
+                  </ServicesListSection>
+               </Side>
+               <Side>
+                  <Img 
+                     fluid={images.imgService.childImageSharp.fluid}
+                     alt="imagem de serviços de reparo mecânico da Shiro Usinagens" 
+                  />
+               </Side>
+            </ServicesListContainer>
+         </Section>
          <Footer />
       </Layout>
    )
---
+++ src/pages/ReparosMecanicos/styles.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import media from "../../styles/breakPoints"
+import { FaArrowRight } from "react-icons/fa"
+
+import Container from "../../objects/Container"
+import TitleSection from "../../objects/TitleSection"
+import TextSection from "../../objects/TextSection"
+
+// History Section
+const ServicesListContainer = styled(Container)`
+   max-width: 1440px;
+   justify-content: space-between;
+   background-color: ${props => props.theme.color.secondyClear};
+
+   ${media.lessThan("md")`
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+   `}
+
+`
+const Side = styled.div`
+   width: 50%;
+   height: auto;
+   &.-content {
+      padding: 0 90px;
+   }
+
+   ${media.lessThan("xl")`
+      &.-content {
+         padding: 0 50px;
+      }
+   `}
+   ${media.lessThan("lg")`
+      &.-content {
+         padding: 0 25px;
+      }
+   `}
+   ${media.lessThan("md")`
+      width: 100%;
+      max-width: 720px;
+      order: 2;
+      &.-content {
+         order: 1;
+         padding: 70px;
+      }
+   `}
+   ${media.lessThan("sm")`
+      &.-content {
+         padding: 70px 25px;
+      }
+   `}
+   ${media.lessThan("xs")`
+      &.-content {
+         padding: 50px 10px;
+      }
+   `}
+
+`
+
+const ServicesTitleSection = styled(TitleSection)`
+
+   ${media.lessThan("xl")`
+      margin-bottom: 0;
+   `}
+
+`
+
+const ServicesListSection = styled(TextSection)`
+
+   ${media.lessThan("xl")`
+      margin-top: 15px;
+   `}
+   ${media.lessThan("lg")`
+      font-size: ${props => props.theme.size.small};
+      line-height: 1.2;
+   `}
+   ${media.lessThan("md")`
+      line-height: 1.5;
+   `}
+
+`
+
+const ListServices = styled.ul`
+   list-style: none;
+   margin-left: 30px;
+`
+
+const ListIcon = styled(FaArrowRight)`
+   margin-right: -15px;
+   transform: translateX(-25px);
+   color: ${props => props.theme.color.firstDark};
+`
+
+
+export {
+   ServicesListContainer,
+   Side,
+   ServicesTitleSection,
+   ServicesListSection,
+   ListServices,
+   ListIcon,
+ }
